54ffedf631c13f28d33b9a7ee8e832f0c3e12513
Pierre Rolland <prolland@lafourchette.com>
2017-03-24 19:01:21 +0100

    Replaced search input by material ui one

--- src/static/filters/searchFilter.js
+++ src/static/filters/searchFilter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
 
 export default React.createClass({
     getInitialState() {
@@ -13,20 +14,26 @@ export default React.createClass({
     },
     render() {
         return (
-            <div className="searchFilter">
-                <div className="input-group">
-                    <span className="input-group-addon">
-                        <span className="glyphicon glyphicon-search"> </span>
-                    </span>
-                    <input
-                        className="form-control input-sm"
-                        type="text"
-                        placeholder="Search by key, translation, ..."
-                        onChange={this.handleChange}
-                        value={this.state.query}
-                    />
-                </div>
-            </div>
+            <TextField
+                className="searchFilter"
+                floatingLabelText="Search by domain, phrase key, translation, ..."
+                onChange={this.handleChange}
+                value={this.state.query}
+            />
+            // <div className="searchFilter">
+            //     <div className="input-group">
+            //         <span className="input-group-addon">
+            //             <span className="glyphicon glyphicon-search"> </span>
+            //         </span>
+            //         <input
+            //             className="form-control input-sm"
+            //             type="text"
+            //             placeholder="Search by key, translation, ..."
+            //             onChange={this.handleChange}
+            //             value={this.state.query}
+            //         />
+            //     </div>
+            // </div>
         );
     }
 });
